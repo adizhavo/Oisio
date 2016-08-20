d37a7d61ea55d4ed3489edf6a53d92a045e8bb96
Adi Zhavo <adi@gram.gs>
2016-08-20 15:00:58 +0300

    Added collectables and arrow collectable prefab.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour 
+{
+    [SerializeField] private CollectableStatusBar statusBar;
+
+    public enum State
+    {
+        Charged,
+        Charging
+    }
+
+    private State current;
+    [SerializeField] private float collectionTime;
+    [SerializeField] private float reloadTime;
+
+    private float percentage = 1f;
+
+    private void Awake()
+    {
+        statusBar.Init(percentage);
+    }
+
+    public void Collect()
+    {
+        if (current.Equals(State.Charging)) return;
+
+        percentage -= Time.deltaTime/collectionTime;
+    }
+
+    private void CheckStatus()
+    {
+        if (percentage < Mathf.Epsilon && current.Equals(State.Charged))
+        {
+            current = State.Charging;
+        }
+        else
+        {
+            percentage += Time.deltaTime/reloadTime;
+            if (percentage > 1) current = State.Charged;
+        }
+
+        percentage = Mathf.Clamp01(percentage);
+    }
+
+    private void Update()
+    {
+        CheckStatus();
+        statusBar.SetBarView(percentage, current);
+    }
+}
