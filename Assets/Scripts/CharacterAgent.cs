e0527bf68136fdebe213178c3c8b2de6511ac282
Adi Zhavo <adi@gram.gs>
2016-08-20 11:03:56 +0300

    Added input config, small refactoring.

--- Assets/Scripts/CharacterAgent.cs
+++ Assets/Scripts/CharacterAgent.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterAgent : MonoBehaviour {
-
+public class CharacterAgent : MonoBehaviour
+{
     public NavMeshAgent agent;
-    public LayerMask grounMask;
 
     public void Update()
     {
-        float xDirection = Input.GetAxis("Horizontal");
-        float yDirection = Input.GetAxis("Vertical");
-
-        Vector3 moveDirection = transform.position + new Vector3(xDirection, 0, yDirection);
+        Vector3 moveDirection = transform.position + new Vector3(InputConfig.XDriection(), 0, InputConfig.YDriection());
         agent.SetDestination(moveDirection);
     }
 }
---
+++ Assets/Scripts/InputConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InputConfig 
+{
+    public static float XDriection()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    public static float YDriection()
+    {
+        return Input.GetAxis("Vertical");
+    }
+}
