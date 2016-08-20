ef37fcddc7d14ca1eecfc2bc8ea7d9e27222842c
Adi Zhavo <adi@gram.gs>
2016-08-20 18:06:45 +0300

    Scene update.

---
+++ Assets/Scripts/DestroyDelay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyDelay : MonoBehaviour
+{
+    [SerializeField] private float destroyAfter = 1;
+
+    private IEnumerator Start () 
+    {
+        yield return new WaitForSeconds(destroyAfter);
+
+        Destroy(gameObject);
+	}
+}
