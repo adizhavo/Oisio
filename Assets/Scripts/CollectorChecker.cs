1482e439f203e47a8179793ffa378438552c9a68
Adi Zhavo <adi@gram.gs>
2016-08-20 16:46:06 +0300

    Moved collector checker into separate source file.

---
+++ Assets/Scripts/CollectorChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CollectorChecker
+{
+    private Collector subscribedEntity;
+    public float CollectorRange = 0.5f;
+
+    public CollectorChecker(Collector subscribedEntity)
+    {
+        this.subscribedEntity = subscribedEntity;
+    }
+
+    public void FrameUpdate()
+    {
+        ResourceCollectable[] resources = Resources.FindObjectsOfTypeAll<ResourceCollectable>();
+
+        foreach(ResourceCollectable ctb in resources)
+        {
+            float distance = Vector3.Distance(ctb.WorlPos, subscribedEntity.WorlPos);
+            if (ctb.gameObject.activeInHierarchy && distance < CollectorRange)
+            {
+                subscribedEntity.Notify(ctb);
+                return;
+            }
+        }
+    }
+}
