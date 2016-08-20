c076230d604c826ec6ce2e54ec55b3b919fd7ced
Adi Zhavo <adi@gram.gs>
2016-08-20 11:00:51 +0300

    Added character movement and pathfinding.

---
+++ Assets/Scripts/CharacterAgent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAgent : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public LayerMask grounMask;
+
+    public void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float yDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.position + new Vector3(xDirection, 0, yDirection);
+        agent.SetDestination(moveDirection);
+    }
+}
