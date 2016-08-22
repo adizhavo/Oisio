b87122d99566481bcaf9828a05eb2eab9c228eeb
Adi Zhavo <adi@gram.gs>
2016-08-22 22:24:33 +0300

    Added mapBlock, used to get position nearby blocks.

---
+++ Assets/Scripts/MapBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MapBlock : MonoBehaviour 
+{    
+    [SerializeField] private float areaRadius;
+
+    public Vector3 GetPositionInArea()
+    {
+        Vector2 randomPos = Random.insideUnitCircle;
+        return new Vector3(transform.position.x + randomPos.x * areaRadius, 0f, transform.position.z + randomPos.y * areaRadius);
+    }
+
+	private void Update ()
+    {
+        DrawArea();
+	}
+
+    private void DrawArea()
+    {
+        Debug.DrawLine(transform.position, transform.position + new Vector3(areaRadius, 0, 0), Color.red);
+    }
+}
