f1057f882b5edcc5c4282d66dd917af83128a752
Adi Zhavo <adi@gram.gs>
2016-08-23 23:29:05 +0300

    Added giant action states, code will not compile.

---
+++ Assets/Scripts/Giant/GiantActionState.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public abstract class GiantActionState
+{
+    protected GiantEvent[] subcribedEvent;
+    protected GiantAgent giant;
+
+    public GiantActionState(GiantAgent giant, params GiantEvent[] subcribedEvent)
+    {
+        this.giant = giant;
+        this.subcribedEvent =subcribedEvent;
+    }
+
+    public virtual bool IsEventValid(GiantEvent requestedEvent)
+    {
+        foreach(GiantEvent e in subcribedEvent)
+        {
+            if (e.Equals(requestedEvent)) return true;
+        }
+
+        return false;
+    }
+
+    public virtual void Notify(GiantEvent firedEvent)
+    {
+        if (IsEventValid(firedEvent))
+        {
+            ChangeState(firedEvent);
+        }
+    }
+
+    public abstract void FrameFeed();
+    public abstract void ChangeState(GiantEvent firedEvent);
+}
+
+public enum GiantEvent
+{
+    TargetNerby,
+}
---
+++ Assets/Scripts/Giant/IdleState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class IdleState : GiantActionState
+{
+    protected float waitTime = 0f;
+
+    public IdleState(GiantAgent giant, params GiantEvent[] events) : base(giant, events) { }
+
+    #region implemented abstract members of GiantActionState
+    public override void FrameFeed()
+    {
+        if (waitTime < 0f)
+        {
+            giant.GotoNearestResource();
+            waitTime = Random.Range(3f, 7f);
+
+            if (ShouldChangeResource())
+            {
+                giant.GotoRandomResource();
+                waitTime += 5f;
+            }
+        }
+
+        waitTime -= Time.deltaTime;
+    }
+
+    public override void ChangeState(GiantEvent firedEvent)
+    {
+        // Change giant state
+        // if (firedEvent.Equals(GiantEvent.NerbyTarget)) 
+        // giant.ChangeState<Alert>()
+    }
+
+    #endregion
+
+    protected virtual bool ShouldChangeResource()
+    {
+        return Random.Range(0, 2) == 0;
+    }
+}
