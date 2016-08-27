28df25df30ced4bfaebe60eeab1c5adf52237eb0
Adi Zhavo <adi@gram.gs>
2016-08-27 16:49:22 +0300

    Typo fixed.

--- Assets/Scripts/Giant/States/GiantAttackState.cs
+++ Assets/Scripts/Giant/States/GiantAttackState.cs
@@ -41,7 +41,7 @@ public class GiantAttackState : GiantActionState
         }
     }
 
-    protected virtual] void UpdateVisualAttack()
+    protected virtual void UpdateVisualAttack()
     {
         if (timeElapse > attackEventPercentage)
         {
