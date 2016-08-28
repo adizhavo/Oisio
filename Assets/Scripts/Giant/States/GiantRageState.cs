06c9a1a72215f2d3905281edf0f823ab874bc9ff
Adi Zhavo <adi@gram.gs>
2016-08-28 15:41:52 +0300

    Added giant rage state.

---
+++ Assets/Scripts/Giant/States/GiantRageState.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiantRageState : GiantActionState
+{
+    private Vector3? eventPos = null;
+
+    public GiantRageState(GiantAgent giant) : base(giant) { }
+
+    #region implemented abstract members of GiantActionState
+
+    public override void Init()
+    {
+        #if UNITY_EDITOR
+        Debug.Log("Giant enters into Rage state..");
+        #endif
+
+        giant.SetSpeed(SpeedLevel.Rage);
+        giant.AlertLevel = GameConfig.maxAlertLevel;
+    }
+
+    public override void FrameFeed()
+    {
+        MovetoEventPos();
+        CheckAttack();
+    }
+
+    public override void Notify(EventTrigger nerbyEvent)
+    {
+        if (nerbyEvent.subject.Equals(EventSubject.Attack) || nerbyEvent.subject.Equals(EventSubject.NerbyTarget))
+        {
+            RageToEvent(nerbyEvent);
+        }
+    }
+
+    #endregion
+
+    protected virtual void MovetoEventPos()
+    {
+        if (eventPos.HasValue)
+        {
+            giant.WorlPos = eventPos.Value;
+        }
+    }
+
+    private void CheckAttack()
+    {
+        if (!eventPos.HasValue) return;
+
+        float distance = Vector3.Distance(giant.WorlPos, eventPos.Value);
+        if (distance < giant.AttackRange)
+        {
+            Attack();
+        }
+    }
+
+    protected virtual void Attack()
+    {
+        giant.Stop();
+        eventPos = null;
+        giant.ChangeState<GiantAttackState>();
+    }
+
+    private void RageToEvent(EventTrigger nerbyEvent)
+    {
+        Init();
+        eventPos = nerbyEvent.WorlPos;
+    }
+}
