a3a0acf8b991fe61bceff1d3f8c4af51e8ab3558
Adi Zhavo <adi@gram.gs>
2016-09-02 15:24:28 +0300

    Movement component consumes stamina while running.

--- Assets/Scripts/Character/Character Components/AgentMovement.cs
+++ Assets/Scripts/Character/Character Components/AgentMovement.cs
@@ -1,18 +1,49 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class AgentMovement : AgentComponent
 {
+    private AgentStamina staminaComponent;
+
+    private float staminaCost
+    {
+        get 
+        {
+            return GameConfig.staminaCost * Time.deltaTime;
+        }
+    }
+
     public AgentMovement(CharacterAgent agent) : base (agent) { }
 
     #region implemented abstract members of AgentComponent
     public override void FrameFeed()
     {
-        float runningSpeed = InputConfig.Run() ? GameConfig.maxCharacterSpeed : GameConfig.minCharacterSpeed;
-        agent.navMeshAgent.speed = runningSpeed;
+        if (staminaComponent == null) staminaComponent = agent.RequestComponent<AgentStamina>();
+        agent.navMeshAgent.speed = AgentSpeed();
+        MoveAgent();
+    }
+    #endregion
 
+    private float AgentSpeed()
+    {
+        bool isRunning = InputConfig.Run() && HasEnoughStamina();
+        float speed = isRunning ? GameConfig.maxCharacterSpeed : GameConfig.minCharacterSpeed;
+
+        if (InputConfig.Run())
+        {
+            staminaComponent.ConsumeStamina(staminaCost);
+        }
+
+        return speed;
+    }
+
+    private bool HasEnoughStamina()
+    {
+        return staminaComponent != null && staminaComponent.stamina > staminaCost;
+    }
+
+    private void MoveAgent()
+    {
         Vector3 moveDirection = agent.transform.position + new Vector3(InputConfig.XDriection(), 0, InputConfig.YDriection());
         agent.navMeshAgent.SetDestination(moveDirection);
     }
-    #endregion
 }
