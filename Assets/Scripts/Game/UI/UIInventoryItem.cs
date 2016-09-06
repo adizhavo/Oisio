2c79e5504d815f3a3931303f81b35dc8c389de4a
Adi Zhavo <adi@gram.gs>
2016-09-06 22:05:07 +0300

    Added item UI counter.

---
+++ Assets/Scripts/Game/UI/UIInventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInventoryItem : MonoBehaviour
+{
+    [SerializeField] private ConsumableType item;
+    [SerializeField] private Agent inventoryCarrier;
+
+    [Header("UI Dependecies")]
+    [SerializeField] private Text itemCount;
+
+    private Inventory inventoryComponent;
+
+	private void Start ()
+    {
+        inventoryComponent = inventoryCarrier.RequestComponent<Inventory>();
+	}
+
+    private void Update ()
+    {
+        if (inventoryComponent == null) return;
+
+        itemCount.text = inventoryComponent.GetItemStock(item).ToString();
+	}
+}
