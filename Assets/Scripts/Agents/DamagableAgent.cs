d756d7de6947d2cfbb8364ed3463314940b2b971
Adi Zhavo <adi@gram.gs>
2016-09-11 07:39:05 +0300

    Added damage agent.

---
+++ Assets/Scripts/Agents/DamagableAgent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DamagableAgent : Agent
+{
+    [Header("Health config")]
+    public float maxHealth;
+    public float healthRegen;
+}
