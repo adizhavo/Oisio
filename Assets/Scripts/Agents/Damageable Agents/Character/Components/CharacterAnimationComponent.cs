1d9b6e194bee85ebb5c8f5f2c6fcee02b0d9c3d5
Adi Zhavo <adi@gram.gs>
2016-09-20 23:23:59 +0300

    Added camera shake while walking or running.

--- Assets/Scripts/Agents/Damageable Agents/Character/Components/CharacterAnimationComponent.cs
+++ Assets/Scripts/Agents/Damageable Agents/Character/Components/CharacterAnimationComponent.cs
@@ -4,15 +4,35 @@ using System.Collections;
 public class CharacterAnimationComponent : CharacterComponent
 {
     public static readonly string RunKey = "run";
+    private CharacterMovementComponent movementComp;
 
     public CharacterAnimationComponent(CharacterAgent agent) : base (agent) { }
 
     #region implemented abstract members of AgentComponent
 
+    private bool isRunning
+    {
+        get 
+        {
+            if (movementComp == null) movementComp = agent.RequestComponent<CharacterMovementComponent>();
+            return movementComp.isRunning;
+        }
+    }
+
+    private bool isWalking
+    {
+        get 
+        {
+            if (movementComp == null) movementComp = agent.RequestComponent<CharacterMovementComponent>();
+            return movementComp.isMoving;
+        }
+    }
+
     public override void FrameFeed()
     {
-        float runningSpeed = InputConfig.Run() ? agent.runSpeed : agent.walkSpeed;
+        float runningSpeed = isRunning ? agent.runSpeed : agent.walkSpeed;
         SetRun(runningSpeed);
+        UpdateCameraShake();
     }
 
     #endregion
@@ -20,5 +40,20 @@ public class CharacterAnimationComponent : CharacterComponent
     protected virtual void SetRun(float speed)
     {
         agent.characterAnimator.SetFloat(RunKey, speed);
+    }
+
+    private bool currentRunning = false;
+
+    private void UpdateCameraShake()
+    {
+        if (currentRunning != isRunning)
+        {
+            currentRunning = isRunning;
+            if (currentRunning) CameraShake.Instance.StartShake(ShakeType.Run);
+            else CameraShake.Instance.StopShake(ShakeType.Run);
+        }
+
+        if (!currentRunning && isWalking) CameraShake.Instance.StartShake(ShakeType.Walk);
+        else CameraShake.Instance.StopShake(ShakeType.Walk);
     }
 }
