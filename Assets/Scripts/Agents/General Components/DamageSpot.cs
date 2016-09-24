449248abd32fb58548bcecf883b288d4c9ca57e8
Adi Zhavo <adi@gram.gs>
2016-09-24 16:37:37 +0300

    Add in game hit effects for normal and critic hit

--- Assets/Scripts/Agents/General Components/DamageSpot.cs
+++ Assets/Scripts/Agents/General Components/DamageSpot.cs
@@ -5,10 +5,28 @@ public class DamageSpot : MonoBehaviour, Damageable, EventListener
     [SerializeField] private MonsterAgent giant;
 
     public float DamageResist;
+    public GameObjectPool HitEffect;
 
     #region Damagable implementation
 
     public void ApplyDamage(float damage)
+    {
+        DamageMonster(damage);
+        DisplayHitEffect(null);
+    }
+
+    public void ApplyDamage(float damage, Vector3 hitPos)
+    {
+        DamageMonster(damage);
+        Vector3? effectPos = hitPos;
+        DisplayHitEffect(effectPos);
+    }
+
+    #endregion
+
+    #region EventListener implementation
+
+    private void DamageMonster(float damage)
     {
         if (giant == null) return;
 
@@ -19,10 +37,6 @@ public class DamageSpot : MonoBehaviour, Damageable, EventListener
             healthComp.ApplyDamage(damageReceived);
         }
     }
-
-    #endregion
-
-    #region EventListener implementation
 
     public void Notify(EventTrigger visibleAction)
     {
@@ -40,4 +54,13 @@ public class DamageSpot : MonoBehaviour, Damageable, EventListener
     public Vector3 WorlPos { set; get; }
 
     #endregion
+
+    private void DisplayHitEffect(Vector3? effectPos)
+    {
+        if (!effectPos.HasValue) return;
+
+        Transform hitEff = PooledObjects.Instance.RequestGameObject(HitEffect).transform;
+        hitEff.transform.position = effectPos.Value;
+        hitEff.LookAt(giant.transform.position);
+    }
 }
