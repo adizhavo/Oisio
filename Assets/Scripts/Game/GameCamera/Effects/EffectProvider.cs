8c1fd015cf98279d8333cd105df6f7e90c2846a5
Adi Zhavo <adi@gram.gs>
2016-10-16 11:12:58 +0300

    Add effects provider and hit effect

---
+++ Assets/Scripts/Game/GameCamera/Effects/EffectProvider.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+namespace Oisio
+{
+    public class EffectProvider : MonoBehaviour
+    {
+
+        public static EffectProvider instance
+        {
+            private set;
+            get;
+        }
+
+        [SerializeField] private VignetteAndChromaticAberration vignetteAndChroma;
+
+        private IEffect[] availableEffects;
+
+        private void Awake()
+        {
+            instance = this;
+            availableEffects = InitEffects();
+        }
+
+        private void Update()
+        {
+            foreach(IEffect effect in availableEffects)
+                effect.FrameFeed();
+        }
+
+        protected virtual IEffect[] InitEffects()
+        {
+            return new IEffect[]
+            {
+                new HitEffect(vignetteAndChroma)
+            };
+        }
+
+        public void DisplayEffect<T>() where T : IEffect
+        {
+            foreach(IEffect effect in availableEffects)
+                if (effect is T)
+                    effect.Display();
+        }
+    }
+}
