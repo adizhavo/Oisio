381b8940df5d60ede83aaa16f06adaae371fe015
Adi Zhavo <adi@gram.gs>
2016-10-16 11:36:54 +0300

    Add sprite health bar

---
+++ Assets/Scripts/Game/UI/HealthBars/SpriteHealthBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Oisio.Game
+{
+    public class SpriteHealthBar : HealthBar
+    {
+        [SerializeField] private Transform pivot;
+        
+    	protected override void Update()
+        {
+            base.Update();
+            pivot.localScale = new Vector3(percentage, 1f, 1f);
+            transform.eulerAngles = Vector3.zero;
+    	}
+    }
+}
