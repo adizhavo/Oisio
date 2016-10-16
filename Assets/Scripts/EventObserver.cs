5c056e31ee2f377cd0882b68a411ab183d0efe82
Adi Zhavo <adi@gram.gs>
2016-10-16 15:43:23 +0300

    Remove monster layer

--- Assets/Scripts/EventObserver.cs
+++ Assets/Scripts/EventObserver.cs
@@ -38,7 +38,7 @@ namespace Oisio.Events
                 }
 
                 float distance = Vector3.Distance(listener.WorlPos, act.WorlPos);
-                bool isVisible = !Physics.Linecast(listener.WorlPos, act.WorlPos, GameConfig.EVENT_OBSERVER_IGNORE_MASK) && distance < listener.VisibilityRadius;
+                bool isVisible = !Physics.Linecast(listener.WorlPos, act.WorlPos) && distance < listener.VisibilityRadius;
 
                 if(isVisible && distance < minDistance && (highpriorityAction == null || highpriorityAction.Priority <= act.Priority) && !act.hasExpired)
                 {
